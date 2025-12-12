bdc20826b198d879258e576a0f069b0f40c019f5
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-12 15:24:25 +0530

    Create HomePage.jsx

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <div className='border w-full h-screen sm:px-[25%] sm:py-[25%]'>
+
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
