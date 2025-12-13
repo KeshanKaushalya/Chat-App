371dc5a9d2a873d1852c83cf7a8a677ea713c9e4
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-13 21:07:22 +0530

    Update ProfilePage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 const ProfilePage = () => {
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+      <div>
+        <form></form>
+        <img src={assets.logo1} alt="" />
+      </div>
     </div>
   )
 }
